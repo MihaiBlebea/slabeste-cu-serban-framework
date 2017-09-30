77a75d6b503cbd1fb30c59e254ca46b15fab4c81
Serban Blebea <slabestecuserban@gmail.com>
2017-09-30 22:57:02 +0100

    work on active campaign integration

--- src/Autoresponder/Autoresponder.php
+++ src/Autoresponder/Autoresponder.php
@@ -18,48 +18,65 @@ class Autoresponder
         $this->ac = new \ActiveCampaign($this->config["active_campaign_api_url"], $this->config["active_campaign_api_key"]);
     }
 
-    // public function getContactDetails($contactId)
-    // {
-    //     $contact = array(
-    // 		"ID" => $contactId,
-    // 	);
-    //
-    // 	$contact_sync = $this->ac->api("contact/view", $contact);
-    //     dd($contact_sync);
-    // }
+    private function getIdFromConfig($type, $name)
+    {
+        return $this->config[$type][$name];
+    }
+
+    // Get get contact details
+    public function getContactDetails($contactId)
+    {
+        return $response = $ac->api("contact/view?id=" . $contactId);
+    }
+
+    // Get active lists with details
+
+    // Get automations
+
+    // Get subsribers today
+    private function getLeadsForDate()
+    {
+        $response = $ac->api("contact/list?ids=all&sort=datetime&sort_direction=DESC&page=1");
+        return count($response);
+    }
+
 
     public function addToList(string $list, $name, $email)
     {
+        // $listId = getIdFromConfig("lists_ID", $list);
+
         $contact = array(
     		"email"                 => $email,
     		"first_name"            => $name,
-    		// "last_name"          => "Test",
     		"p[" . $list . "]"      => $list,
     		"status[" . $list . "]" => 1
     	);
 
-    	$contact_sync = $this->ac->api("contact/sync", $contact);
-
-    	if (!(int)$contact_sync->success)
-        {
-            dd($contact_sync->error);
-    		// request failed
-    		// echo "<p>Syncing contact failed. Error returned: " . $contact_sync->error . "</p>";
-    		return false;
-    	}
-
-        // successful request
-        return (int)$contact_sync->subscriber_id;
+    	return $contact_sync = $this->ac->api("contact/sync", $contact);
     }
 
     public function addToAutomation($contactId, string $automation)
     {
+        // $automationId = getIdFromConfig("automations_ID", $automation);
+
         $post_data = array(
             "contact_id" => $contactId, // include this or contact_id
             "automation" => $automation, // one or more
         );
-        $response = $this->ac->api("automation/contact/add", $post_data);
-        return $response;
+        return $response = $this->ac->api("automation/contact/add", $post_data);
+    }
+
+    public function addTags($contactId, array $tags)
+    {
+        $schema = "";
+        foreach($tags as $index => $tag)
+        {
+            $schema .= "tags[" . $index . "]": $tag,
+        }
+
+        $data = get_object_vars(json_decode('{"id": "' . $contactId . '", ' . $schema . '}'));
+
+        return $response = $ac->api("contact/tag/add", $data);
     }
 
 }
